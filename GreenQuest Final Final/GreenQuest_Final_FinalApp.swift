4f84c00e5375ae587f45b063912fe634e4977f56
Shaan Duggal <shaanduggal10@icloud.com>
2024-05-28 10:59:25 -0700

    Initial Commit

---
+++ GreenQuest Final Final/GreenQuest_Final_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenQuest_Final_FinalApp.swift
+//  GreenQuest Final Final
+//
+//  Created by Shaan Duggal on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct GreenQuest_Final_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
